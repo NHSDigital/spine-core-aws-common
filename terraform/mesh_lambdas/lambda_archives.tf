4b4ed7f14dea1fe345d3f1c822993aa01c282a3f
Joe Stringer <joe.stringer2@nhs.net>
2021-07-09 09:45:04 +0100

    Update lambdas

--- terraform/mesh_lambdas/lambda_archives.tf
+++ terraform/mesh_lambdas/lambda_archives.tf
@@ -1,6 +1,6 @@
 data "archive_file" "mesh_implementation_lambdas" {
   type        = "zip"
-  source_dir  = "${path.module}/../../spine_aws_common/mesh/mesh_implementation_lambdas"
+  source_dir  = "${path.module}/../../mesh_implementation_lambdas"
   output_path = "${path.module}/mesh_implementation_lambdas.zip"
 }
 
