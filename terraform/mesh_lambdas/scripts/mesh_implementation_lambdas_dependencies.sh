c01b3b65ed37b1a2260714609ffb4c2b195bd2c8
Joe Stringer <joe.stringer2@nhs.net>
2021-07-13 16:00:23 +0100

    Cleanup of deps build script

--- terraform/mesh_lambdas/scripts/mesh_implementation_lambdas_dependencies.sh
+++ terraform/mesh_lambdas/scripts/mesh_implementation_lambdas_dependencies.sh
@@ -9,35 +9,29 @@ set -o pipefail
 # deterministic dir
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 cd ${SCRIPT_DIR}
+DEPS_DIR="../mesh_implementation_lambdas_dependencies/python"
+PYTHON_BIN="python3.8"
 
-# Check for python3.8 and zip
-which python
-python -V | grep 3.8
+# Check for python and zip
+which ${PYTHON_BIN}
 which zip
 
-# Create venv
-python -m venv venv
-source venv/bin/activate
+# Create target dir
+mkdir -p ${DEPS_DIR}
+rm -rf ${DEPS_DIR}/*
 
 # Install deps
-python -m pip install --upgrade pip
-python -m pip install \
+# TODO is there a requirements.txt?
+${PYTHON_BIN} -m pip install --upgrade pip
+${PYTHON_BIN} -m pip download --dest ${DEPS_DIR} \
   aws_lambda_powertools \
   mesh_client \
   requests \
   urllib3
 
-# Create target dir
-mkdir -p ../mesh_implementation_lambdas_dependencies/python
-rm -rf ../mesh_implementation_lambdas_dependencies/python/*
-
-# do this as we cannot cp a parent dir to sub dir
-mkdir -p /tmp/spine_aws_common
-cp -r ../../../spine_aws_common/* /tmp/spine_aws_common
-mv /tmp/spine_aws_common ../mesh_implementation_lambdas_dependencies/python/spine_aws_common
-
-# copy packages from venv
-cp -r ./venv/lib/python3.8/site-packages/* ../mesh_implementation_lambdas_dependencies/python/
+# get the spine common library
+mkdir -p ${DEPS_DIR}/spine_aws_common
+cp -r ../../../spine_aws_common/* ${DEPS_DIR}/spine_aws_common
 
 # This will then be zipped by terraform
 exit 0
