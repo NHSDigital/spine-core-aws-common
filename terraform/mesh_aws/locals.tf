50729fe88691a2b1fd1c63efe4d8dd5a444f9aa5
Joe Stringer <joe.stringer2@nhs.net>
2021-07-16 09:02:40 +0100

    Correct Mesh Prod URL

--- terraform/mesh_aws/locals.tf
+++ terraform/mesh_aws/locals.tf
@@ -3,7 +3,7 @@ locals {
 
   mesh_url = {
     integration = "https://msg.intspineservices.nhs.uk"
-    production  = "https://msg.spineservices.nhs.uk"
+    production  = "https://mesh-sync.spineservices.nhs.uk"
   }
 
   python_runtime = "python3.8"
