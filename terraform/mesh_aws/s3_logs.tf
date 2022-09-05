5ba5670d064f1727f9e5750bd6ab1b59898cbf65
johnwaghorn <john.waghorn2@nhs.net>
2022-09-05 20:09:05 +0100

    enforce-ssl-on-mesh-buckets

--- terraform/mesh_aws/s3_logs.tf
+++ terraform/mesh_aws/s3_logs.tf
@@ -33,6 +33,29 @@ resource "aws_s3_bucket_public_access_block" "s3logs" {
 
 data "aws_iam_policy_document" "s3logs" {
   statement {
+    sid = "AllowSSLRequestsOnly"
+    actions = [
+      "s3:*",
+    ]
+    effect = "Deny"
+    resources = [
+      "arn:aws:s3:::${local.name}-s3logs",
+      "arn:aws:s3:::${local.name}-s3logs/*",
+    ]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    condition {
+      test = "Bool"
+      values = [
+        "false",
+      ]
+
+      variable = "aws:SecureTransport"
+    }
+  }
+  statement {
     sid    = "AWSCloudTrailAclCheck"
     effect = "Allow"
 
