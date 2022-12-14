e761b2dcb2c1668c00816c8964dc83d26895e490
Luke Carroll <luke.carroll1@nhs.net>
2022-12-14 15:55:21 +0000

    PCRM-6842 attempting to fix 'requests.packages' has no 'urllib3' member error.

--- mesh_aws_client/mesh_mailbox.py
+++ mesh_aws_client/mesh_mailbox.py
@@ -90,7 +90,7 @@ class MeshMailbox:  # pylint: disable=too-many-instance-attributes
             self.params.get(MeshMailbox.MESH_VERIFY_SSL, False) == "True"
         )
         if not self.maybe_verify_ssl:
-            requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+            urllib3.disable_warnings(InsecureRequestWarning)
         self._write_certs_to_files()
 
     def clean_up(self) -> None:
