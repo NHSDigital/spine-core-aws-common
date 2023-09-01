bb743568785f72bf131e15b4f5d02f61b8333dd1
TeiturArgeSeloy <130432976+TeiturArgeSeloy@users.noreply.github.com>
2023-09-01 20:48:28 +0200

    change count check from string to boolean

--- terraform/mesh_aws/s3_logs.tf
+++ terraform/mesh_aws/s3_logs.tf
@@ -1,16 +1,6 @@
 #tfsec:ignore:aws-cloudtrail-require-bucket-access-logging tfsec:ignore:aws-s3-enable-versioning
 resource "aws_s3_bucket" "s3logs" {
   bucket = "${local.name}-s3logs"
-  acl    = "log-delivery-write"
-  policy = data.aws_iam_policy_document.s3logs.json
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 
   lifecycle_rule {
     id      = "AWSLogs"
@@ -30,6 +20,14 @@ resource "aws_s3_bucket_public_access_block" "s3logs" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3logs_controls" {
+  bucket = aws_s3_bucket.s3logs.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 data "aws_iam_policy_document" "s3logs" {
