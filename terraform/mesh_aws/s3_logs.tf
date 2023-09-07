3cb12727541b322bd811d2f22280867b7e6e0882
TeiturArgeSeloy <130432976+TeiturArgeSeloy@users.noreply.github.com>
2023-09-07 10:06:18 +0200

    add aws_s3bucket_acl

--- terraform/mesh_aws/s3_logs.tf
+++ terraform/mesh_aws/s3_logs.tf
@@ -1,16 +1,6 @@
 #tfsec:ignore:aws-cloudtrail-require-bucket-access-logging tfsec:ignore:aws-s3-enable-versioning
 resource "aws_s3_bucket" "s3logs" {
   bucket = "${local.name}-s3logs"
-  acl    = "log-delivery-write"
-  policy = data.aws_iam_policy_document.s3logs.json
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 
   lifecycle_rule {
     id      = "AWSLogs"
@@ -30,6 +20,26 @@ resource "aws_s3_bucket_public_access_block" "s3logs" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3logs_controls" {
+  bucket = aws_s3_bucket.s3logs.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "s3logs_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.s3logs_controls]
+
+  bucket = aws_s3_bucket.s3logs.id
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_policy" "s3logs_policy" {
+  bucket = aws_s3_bucket.s3logs.id
+  policy = data.aws_iam_policy_document.s3logs.json
 }
 
 data "aws_iam_policy_document" "s3logs" {
